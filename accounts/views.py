23dc824b6cb447482c7ce955b1fd913667d7cc5a
ahmed samy <ahmedsamyaaa10@gmail.com>
2020-02-13 18:35:36 +0200

    firts accounts commit 2

---
+++ accounts/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib  import auth
+
+
+
+def home(request):
+    return    render(request, 'accounts/home.html')
+
+
+
+def signup (request):
+    if request.method == 'POST' :
+        if request.POST['password'] ==request.POST['password2']:
+            try:
+                    user = User.objects.get(username=request.POST['username'])
+                         return ender (request,'accounts/signup.html' , {'error':'username has already taken'})
+            except User.DoesNotExist:
+                      user =  User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                      auth.login(request,user)
+                        return redirect('home')
+     else:
+
+    return render (request,'accounts/signup.html')
+
+
+def login(request):
+     return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return    render(request, 'accounts/logout.html')
+
+
