021ea465fda84cd083684672cc20873f6cf40871
ahmed samy <ahmedsamyaaa10@gmail.com>
2020-02-13 22:18:36 +0200

    firts accounts commit 4

--- accounts/views.py
+++ accounts/views.py
@@ -4,19 +4,6 @@ from django.contrib  import auth
 
 
 
-def home(request):
-    return    render(request, 'accounts/home.html')
-
-
-
-def login(request):
-     return render(request, 'accounts/login.html')
-
-
-def logout(request):
-    return    render(request, 'accounts/logout.html')
-
-
 
 
 def signup (request):
@@ -24,13 +11,25 @@ def signup (request):
 
         if request.POST['password'] ==request.POST['password2']:
             try:
-                    user = User.objects.get(username=request.POST['username'])
-                    return render (request,'accounts/signup.html' , {'error':'username has already taken'})
+                user = User.objects.get(username=request.POST['username'])
+                return render (request,'accounts/signup.html' , {'error':'username has already taken'})
             except User.DoesNotExist:
-                      user =  User.objects.create_user(request.POST['username'], password=request.POST['password'])
-                      auth.login(request,user)
-            return redirect('home')
+                user =  User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                auth.login(request,user)
+                return redirect('home')
     else:
 
-            return render (request,'accounts/signup.html')
+        return render (request,'accounts/signup.html')
+
+
+def home(request):
+    return render(request, 'accounts/home.html')
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
 
