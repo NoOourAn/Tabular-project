e647ad915de1f300d3efe8e75f53cfafdbef1ecd
ahmed samy <ahmedsamyaaa10@gmail.com>
2020-02-13 21:57:25 +0200

    firts accounts commit 3

--- accounts/views.py
+++ accounts/views.py
@@ -9,21 +9,6 @@ def home(request):
 
 
 
-def signup (request):
-    if request.method == 'POST' :
-        if request.POST['password'] ==request.POST['password2']:
-            try:
-                    user = User.objects.get(username=request.POST['username'])
-                         return ender (request,'accounts/signup.html' , {'error':'username has already taken'})
-            except User.DoesNotExist:
-                      user =  User.objects.create_user(request.POST['username'], password=request.POST['password'])
-                      auth.login(request,user)
-                        return redirect('home')
-     else:
-
-    return render (request,'accounts/signup.html')
-
-
 def login(request):
      return render(request, 'accounts/login.html')
 
@@ -31,4 +16,21 @@ def login(request):
 def logout(request):
     return    render(request, 'accounts/logout.html')
 
+
+
+
+def signup (request):
+    if request.method == 'POST' :
+
+        if request.POST['password'] ==request.POST['password2']:
+            try:
+                    user = User.objects.get(username=request.POST['username'])
+                    return render (request,'accounts/signup.html' , {'error':'username has already taken'})
+            except User.DoesNotExist:
+                      user =  User.objects.create_user(request.POST['username'], password=request.POST['password'])
+                      auth.login(request,user)
+            return redirect('home')
+    else:
+
+            return render (request,'accounts/signup.html')
 
