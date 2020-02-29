5878a74ae03ea33623e1fba7690daf03f6a9c19b
ahmed samy <ahmedsamyaaa10@gmail.com>
2020-02-29 18:53:30 +0200

    xxxxxxxxxxxxxx

--- db convert.py
+++ db convert.py
@@ -20,40 +20,59 @@ class my_dictionary(dict):
     def add(self, key , value):
         self[key]= value
 
+class my_list(list):
+    def __init__(self):
+        self=list()
+
+    def add(self, value1 , value2):
+        list1=list.append(self,value1)
+        list2=list.append(self,value2)
+        # self=list1.append(list2)
+        # li =[]
+        for i in list1,list2:
+            self[i] = list1[i]+list2[i]
 
 
-loc = ('C:/Users/Ahmed_ELSami/Desktop/MINIUNIVDATASET.xlsx')
+
+loc = ('C:/Users/Ahmed_ELSami/Desktop/MINIUNIVDATASET.xlsx')  #cat
 
 wb = xlrd.open_workbook(loc)
 sheet = wb.sheet_by_index(0)
 sheet.cell_value(0, 0)
 dict_obj = my_dictionary()
 
-
-
-
-
 for i in range(sheet.nrows):
 
     subjects =sheet.cell_value(i, 0)
-
    # print(subjects)
-
-    students= sheet.row_values(i)
+    j=1
+    students= sheet.row_values(i,j)
     #print(students)
 
     dict_obj.add(subjects,students)
-
-
 # print(subjects)
 # print(students)
-
 print(dict_obj)
+#print(dict)
+
+loc2 = ('C:/Users/Ahmed_ELSami/Desktop/MINIUNIVROOMSDATASET.xlsx')  #rooms
+
+wb2 = xlrd.open_workbook(loc2)
+sheet2 = wb2.sheet_by_index(0)
+sheet2.cell_value(0, 0)
+dict_obj2 = my_dictionary()
+list_obj= my_list()
+
+
+for i in range(sheet2.nrows):
+    rooms = list(sheet2.cell_value(i, 0))
+    #print(rooms)
+    capacity = list(sheet2.row_values(i,1))
+    #print(capacity)
+    rooms.append(capacity)
+list_obj= my_list()
+list_obj.add(rooms,capacity)
+print(list_obj)
 
 
 
-# StudentOnly = students - subjects
-#
-# print(StudentOnly)
-
-print(dict)
